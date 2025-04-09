5dd1cff8db1cc07eb78f2b3902d508e29e0184e3
d-cuzya <Admin@DESKTOP-T6OG5FF>
2025-04-09 15:22:58 +0300

    final

--- main.cpp
+++ main.cpp
@@ -31,6 +31,30 @@ public:
 		Item* newItem = new Item(_value, first_item);
 		first_item = newItem;
 	}
+	void remove() {
+		Item* tmp = first_item;
+		first_item = first_item->next_pointer;
+		delete(tmp);
+	}
+	void Delete() {
+		Item* tmp = first_item;
+		Item* previus = nullptr;
+		while (tmp->next_pointer != nullptr) {
+			previus = tmp;
+			tmp = tmp->next_pointer;
+		}
+		delete(tmp);
+		previus->next_pointer = nullptr;
+	}
+	int size() {
+		int i = 1;
+		Item* tmp = first_item;
+		while (tmp->next_pointer != nullptr) {
+			tmp = tmp->next_pointer;
+			i++;
+		}
+		return i;
+	}
 	void iterate() {
 		int index = 1;
 		Item* tmpPtr = first_item;
@@ -45,12 +69,24 @@ public:
 
 int main() {
 	SinglyLinkedList singlyLinked;
+	std::cout << "Add to End 4 elements:\n";
 	singlyLinked.append(6);
 	singlyLinked.append(5);
 	singlyLinked.append(9);
 	singlyLinked.append(2);
 	singlyLinked.iterate();
-	std::cout << "============\n";
+	std::cout << "Add to Start 3 elemnts:\n";
 	singlyLinked.insert(2);
+	singlyLinked.insert(10);
+	singlyLinked.insert(-1);
 	singlyLinked.iterate();
+	std::cout << "Delete 2 items from Start:\n";
+	singlyLinked.remove();
+	singlyLinked.remove();
+	singlyLinked.iterate();
+	std::cout << "Delete 2 items from End:\n";
+	singlyLinked.Delete();
+	singlyLinked.Delete();
+	singlyLinked.iterate();
+	std::cout << "Size: " << singlyLinked.size() << "\n";
 }
